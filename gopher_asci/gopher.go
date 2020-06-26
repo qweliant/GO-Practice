9f46f25dba969dc9a48fb659c58e9e508d11b5de
qweliant <qweliantanner@gmail.com>
2020-06-26 11:11:35 -0400

    gopher

---
+++ gopher_asci/gopher.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("      `.-::::::-.`\n")
+	fmt.Printf("  .:-::::::::::::::-:.\n")
+	fmt.Printf("  `_:::    ::    :::_`\n")
+	fmt.Printf("   .:( ^   :: ^   ):.\n")
+	fmt.Printf("   `:::   (..)   :::.\n")
+	fmt.Printf("   `:::::::UU:::::::`\n")
+	fmt.Printf("   .::::::::::::::::.\n")
+	fmt.Printf("   O::::::::::::::::O\n")
+	fmt.Printf("   -::::::::::::::::-\n")
+	fmt.Printf("   `::::::::::::::::`\n")
+	fmt.Printf("   .::::::::::::::.\n")
+	fmt.Printf("      oO:::::::Oo\n")
+
+}

008c8a6bcfc2df1fbf0d5929c59d61c9ee06861b
qweliant <qweliantanner@gmail.com>
2020-06-25 21:53:35 -0400

    Here we GOgit add --all

---
+++ README.md
@@ -0,0 +1,13 @@
+#### Go practice and code acdemy tutorial
+
+to build an executable run: 
+
+`go build [FILENAME].go`
+
+`./[FILENAME]`
+
+you can also run:
+
+`go run [FILENAME].go`
+
+if you do not want to repeatedly build executable
--- messing_around/messing_around.go
+++ messing_around/messing_around.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	t "time"
+)
 
 func main() {
+
+	// initialize array. arrays must be initialized to appropriate size
 	var n [10]int
 	var i, j int
-	/* initialize elements of array n to 0 */
+	/* initialize elements of array n to 0.......no parenthesis!!!!!!!!!!!*/
 	for i = 0; i < 10; i++ {
 		n[i] = i + 100 /* set element at location i to i + 100 */
 	}
@@ -14,4 +19,10 @@ func main() {
 	for j = 0; j < 10; j++ {
 		fmt.Printf("Element[%d] = %d\n", j, n[j])
 	}
+
+	// print time
+	fmt.Println(t.Now())
+
+	
+
 }

679a3a30554deea4cdb370c398635a7731a541d2
qweliant <qweliantanner@gmail.com>
2020-07-07 23:09:13 -0400

    array

--- algorithms/linear_table.go
+++ algorithms/linear_table.go
@@ -3,3 +3,14 @@ package main
 import "fmt"
 
 // Creating a one dimensional array
+func main(){
+	var scores = []int8{90, 70, 50, 80, 60, 85}
+
+	// length variable
+	var length = len(scores)
+
+	// for loop printing elements in array
+	for i := 0; i < length; i++{
+		fmt.Printf("%d", scores[i])
+	}
+}

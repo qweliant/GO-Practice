f7a1b35465649685c3a36e9a2c8a3d37588c5bba
Qwelian Tanner <qweliantanner@gmail.com>
2020-07-13 23:44:32 -0400

    select sort debug

---
+++ algorithms/select_sort.go
@@ -0,0 +1,42 @@
+package main
+import "fmt"
+
+func sort(arr []int, length int) {
+	
+	var minIndex int
+	
+	for i := 0; i < length-1; i++ {
+		
+		minIndex = i
+		
+		var minValue = arr[minIndex]
+		
+		for j := 0; j < length-1; j++ {
+			
+			if minValue > arr[j+1] {
+				minValue = arr[j+1]
+				minIndex = j + 1
+			}
+		}
+		
+		if i != minIndex {
+			var temp = arr[i]
+			arr[i] = arr[minIndex]
+			arr[minIndex] = temp
+		}
+	}
+}
+
+func main() {
+	var scores = []int{90, 70, 50, 80, 60, 85}
+
+	// length variable
+	var length = len(scores)
+
+	sort(scores, length)
+
+	// for loop printing elements in array
+	for i := 0; i < length; i++ {
+		fmt.Printf("%d ", scores[i])
+	}
+}

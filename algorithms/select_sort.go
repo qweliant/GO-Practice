6cc53e43888b0a3cd27d01c7217874b78a45f1d4
Qwelian Tanner <qweliantanner@gmail.com>
2020-07-13 23:50:54 -0400

    debug done

--- algorithms/select_sort.go
+++ algorithms/select_sort.go
@@ -1,31 +1,6 @@
 package main
 import "fmt"
 
-func sort(arr []int, length int) {
-	
-	var minIndex int
-	
-	for i := 0; i < length-1; i++ {
-		
-		minIndex = i
-		
-		var minValue = arr[minIndex]
-		
-		for j := 0; j < length-1; j++ {
-			
-			if minValue > arr[j+1] {
-				minValue = arr[j+1]
-				minIndex = j + 1
-			}
-		}
-		
-		if i != minIndex {
-			var temp = arr[i]
-			arr[i] = arr[minIndex]
-			arr[minIndex] = temp
-		}
-	}
-}
 
 func main() {
 	var scores = []int{90, 70, 50, 80, 60, 85}
@@ -40,3 +15,29 @@ func main() {
 		fmt.Printf("%d ", scores[i])
 	}
 }
+
+func sort(arr []int, length int) {
+	
+	var minIndex int
+	
+	for i := 0; i < length-1; i++ {
+		
+		minIndex = i
+		var minValue = arr[minIndex]
+		
+		for j := i; j < length-1; j++ {
+			if minValue > arr[j+1] {
+				minValue = arr[j+1]
+				minIndex = j + 1
+			}
+		}
+		
+		if i != minIndex {
+			var temp = arr[i]
+			arr[i] = arr[minIndex]
+			arr[minIndex] = temp
+		}
+	}
+}
+
+

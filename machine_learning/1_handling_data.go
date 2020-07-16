40a4da4152d976d552396d455f48e1f922870aad
Qwelian Tanner <qweliantanner@gmail.com>
2020-07-15 20:04:35 -0400

    processing data

--- machine_learning/1_handling_data.go
+++ machine_learning/1_handling_data.go
@@ -3,12 +3,30 @@ package main
 import (
 	"encoding/csv"
 	"io"
-
 	"fmt"
 	"log"
 	"os"
-	// "strconv"
+	"strconv"
 )
+
+type CSVRecord struct {
+	id int32
+	name string
+	date string
+	manner_of_death string
+	armed string
+	age int16
+	gender string
+	race string
+	city string
+	state string
+	signs_of_mental_illness bool
+	threat_level string
+	flee string
+	body_camera bool
+	ParseError error
+}
+
 
 func main() {
 
@@ -21,9 +39,9 @@ func main() {
 
 	// Read in csv
 	df := csv.NewReader(f)
-
+	var csvData = []CSVRecord
 	// FieldsPerRecord is set negative in case each row has a variable number of fields
-	df.FieldsPerRecord = -1
+	df.FieldsPerRecord = 14
 
 	// rawCSVData will hold our
 	// reading df in chunk, cool but doesnt handle typing
@@ -32,14 +50,73 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 
+	
 	var raw [][]string
 
+	// iterate through each row to check types
 	for {
 
 		record, err := df.Read()
 		if err == io.EOF {
 			break
 		}
+
+		var csvRecord CSVRecord
+		const check =  0 
+		for idx, value:= range record{
+
+			// check types
+			if idx == 0 {
+				if value < check {
+					log.Printf("Unexpected type in column %d\n", idx)
+					csvRecord.ParseError = fmt.Errorf("!!Empty string value!!")
+					break
+				}
+				
+				csvRecord.id = value
+				continue
+			}
+
+			if idx == 5 {
+				if value == check {
+					log.Printf("Unexpected type in column %d\n", idx)
+					csvRecord.ParseError = fmt.Errorf("!!Empty string value!!")
+					break
+				}
+				
+				csvRecord.armed = value
+				continue
+			}
+
+			if idx == 10 {
+				if value == "" {
+					log.Printf("Unexpected type in column %d\n", idx)
+					csvRecord.ParseError = fmt.Errorf("!!Empty string value!!")
+					break
+				}
+				if value == "True"{
+					csvRecord.signs_of_mental_illness = true
+					continue
+				}
+				if value == "True"{
+					csvRecord.signs_of_mental_illness = true
+					continue
+				}
+				
+			}
+
+			var colValue string
+			switch idx {
+			case 0:
+				csvRecord.id
+			}
+		}
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		// # append record to dataset
 		raw = append(raw, record)
 
 	}

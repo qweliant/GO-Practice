d6863f94f297a18ecd7a99dd66f568078381eb33
qweliant <qweliantanner@gmail.com>
2020-08-17 16:12:48 -0400

    shellsort with wiki implementation

--- algorithms/12_dichotomous_binary_search.go
+++ algorithms/12_dichotomous_binary_search.go
@@ -2,10 +2,9 @@ package main
 
 import "fmt"
 
-
 // Today I will be doing Dichotomous Bin search
 
-func binSearch(array []int, val int) int{
+func binSearch(array []int, val int) int {
 
 	var length = len(array)
 	var low = 0
@@ -13,31 +12,31 @@ func binSearch(array []int, val int) int{
 	var mid = (high + low) / 2
 	fmt.Printf("Indices %d %d %d ", low, mid, high)
 
-	for{
+	for {
 		if array[mid] == val {
 			return mid
-		} else if array[mid] > val{
-			high = mid -1
+		} else if array[mid] > val {
+			high = mid - 1
 			mid = (high + low) / 2
 
-		} else if array[mid] < val{
+		} else if array[mid] < val {
 			low = mid + 1
 			mid = (high + low) / 2
 
-		} else if low >= high{
+		} else if low >= high {
 			fmt.Printf("Not Found")
 			return -1
 		}
 	}
-	return -1
 
 }
 
-func main(){
-	var scores = []int{10, 20 , 30, 40, 50, 60, 70, 80, 95, 210, 590}
+func main() {
+	var scores = []int{10, 20, 30, 40, 50, 60, 70, 80, 95, 210, 590}
 
 	// length variable
 	var res int
 	res = binSearch(scores, 80)
 	fmt.Printf("Result: %d", res)
+
 }
--- algorithms/13_shell_sort.go
+++ algorithms/13_shell_sort.go
@@ -2,11 +2,36 @@ package main
 
 import "fmt"
 
-func shellSort(arraya []int){
+func shellSort(array []int32, length int) {
 
+	var gaps = []int32{1391376, 463792, 198768, 86961, 33936,
+		13776, 4592, 1968, 861, 336,
+		112, 48, 21, 7, 3, 1}
+
+	var r int32 = int32(length)
+	var temp int32
+
+	for k := 0; k < 16; k++ {
+		for i := gaps[k]; i < r; i++ {
+			temp = array[i]
+			var j = i
+			for j = i; j >= gaps[k] && array[j-gaps[k]] > temp; j -= gaps[k] {
+				array[j] = array[j-gaps[k]]
+			}
+			array[j] = temp
+		}
+	}
 }
 
-func main(){
-	var scores = []int{10, 20 , 30, 40, 50, 60, 70, 80, 95, 210, 590}
+func main() {
 
+	var scores = []int32{10, 20, 30, 40, 590, 50, 60, 70, 80, 95, 210}
+	var length = len(scores)
+
+	shellSort(scores, length)
+
+	// for loop printing elements in array
+	for i := 0; i < length; i++ {
+		fmt.Printf("%d ", scores[i])
+	}
 }
